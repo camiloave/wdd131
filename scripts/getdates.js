fb920844d4597ac550e16320838eb2d6b564218f
camiloave <cvergaraescobar@byupathway.edu>
2026-01-12 18:26:13 -0300

    W01 – Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+// Año actual
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Última modificación
+document.getElementById("lastModified").textContent =
+    `Last Modification: ${document.lastModified}`;
